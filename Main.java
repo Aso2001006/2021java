8d575cf6880f833cf5b8976fd96d02ced803cf06
Aso2001006 <83050421+Aso2001006@users.noreply.github.com>
2021-07-26 10:46:04 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package kadai;
+
+public class Main{
+    public static void main(String[] args) {
+        Game game = new Game();
+
+        //初期化
+        game.init();
+        //開始
+        game.execute();
+    }
+}
